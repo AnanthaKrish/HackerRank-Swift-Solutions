55ef183c213f67cfd49d15b980d586f52679ccc1
Ananth <agirijak@in.ibm.com>
2019-03-06 21:07:12 +0530

    Counting values issue

--- counting-valleys.swift
+++ counting-valleys.swift
@@ -1,1 +1,31 @@
 import UIKit
+
+func countingValleys(n: Int, s: String) -> Int {
+    
+    if (n <= 0 && s.count <= 0) {
+        return 0
+    }
+    
+    var str = s
+    var value = 0;
+    var count = 0
+    var isStart = false
+    for i in str {
+        
+        if i == "U" {
+            value += 1
+        } else if i == "D" {
+            value -= 1
+        }
+        if value < 0 && !isStart {
+            count += 1
+            isStart = true
+        }
+        if value >= 0 {
+            isStart = false
+        }
+    }
+    return count
+}
+print(countingValleys(n: 12, s: "DDUUDDUDUUUD"))
+print(countingValleys(n: 8, s: "UDDDUDUU"))
